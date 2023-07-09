b8c8ae2f550e2cf6cf2fb3e71933e7380e28cba8
johnson24694 <118400914+johnson24694@users.noreply.github.com>
2023-07-09 16:24:47 -0500

    update

--- arraysToDo1.js
+++ arraysToDo1.js
@@ -1,39 +1,39 @@
 // Push Front
 // Given an array and an additional value, insert this value at the beginning of the array.
-// push1 = [5,7,2,3], 8
-// push2 = [99], 7
-// push3 = [2,66,85,126,-1,5], 59
+push1 = [5,7,2,3], 8
+push2 = [99], 7
+push3 = [2,66,85,126,-1,5], 59
 
-// function pushFront(arr, value) {
-//     for(var i=arr.length; i>0; i--){
-//         arr[i] = arr[i-1];        
-//     }
-//     arr[0] = value
-//     return(arr);
-// }
+function pushFront(arr, value) {
+    for(var i=arr.length; i>0; i--){
+        arr[i] = arr[i-1];        
+    }
+    arr[0] = value
+    return(arr);
+}
 
-// console.log(pushFront(push1, 8));
-// console.log(pushFront(push2, 7));
-// console.log(pushFront(push3, 59));
+console.log(pushFront(push1, 8));
+console.log(pushFront(push2, 7));
+console.log(pushFront(push3, 59));
 
 
 // Pop Front
 // Given an array, remove and return the value at the beginning of the array. Prove the value is removed from the array by printing it. 
-// pop1 = [0,5,10,15]
-// pop2 = [4,5,7,9]
+pop1 = [0,5,10,15]
+pop2 = [4,5,7,9]
 
-// function popFront (arr) {
-//     let save = arr[0]
-//     for(let i = 0; i <arr.length; i++){
-//         arr[i] = arr[i+1];
-//     }
-//     arr.length = arr.length - 1
-//     console.log(arr)
-//     return save
-// }
+function popFront (arr) {
+    let save = arr[0]
+    for(let i = 0; i <arr.length; i++){
+        arr[i] = arr[i+1];
+    }
+    arr.length = arr.length - 1
+    console.log(arr)
+    return save
+}
 
-// console.log(popFront(pop1));
-// console.log(popFront(pop2));
+console.log(popFront(pop1));
+console.log(popFront(pop2));
 
 // Insert At
 // Given an array, index, and additional value, insert the value into array at given index. You can think of pushFront(arr,val) as equivalent to insertAt(arr,0,val).
