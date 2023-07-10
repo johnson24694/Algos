d60bf127435992fb680c8ac29d9cbf7393599e6d
johnson24694 <118400914+johnson24694@users.noreply.github.com>
2023-07-10 11:40:57 -0500

    updates

--- stringsToDo1.js
+++ stringsToDo1.js
@@ -12,3 +12,12 @@ function removeBlanks(string) {
 console.log(removeBlanks(remove1));
 console.log(removeBlanks(remove2));
 
+// Get Digits
+// Create a JavaScript function that given a string, returns the integer made from the string’s digits. 
+
+digits1 = "abc8c0d1ngd0j0!8"
+digits2 = "0s1a3y5w7h9a2t4?6!8?0"
+
+function getDigits() {
+
+}
